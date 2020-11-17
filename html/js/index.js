4905b5298798955f326b7132609656001a1bddde
Alexiaalurralde <aalurralde@udesa.edu.ar>
2020-11-17 11:13:27 -0300

    avances html y css

---
+++ html/js/index.js
@@ -0,0 +1,37 @@
+//1 construir la url (endpoint). url con APIKey
+let apiKey = 'ca4449919efcaf3d7e435fc10a0a0b0b'
+let url = `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`
+
+//2 construir fetch con la url de datos que queremos mostrar en el html
+fetch(url) 
+
+    .then (function(respuesta){
+        return respuesta.json()
+    })
+
+    .then(function(data){
+        //3. Identificar donde estaá la informaciñon
+        console.log(data.results);
+        let trending = data.results
+
+        //3.1 Capturar el destino a donde queremos mostrar los datos.
+        let sectionTrending = document.querySelector('.estrenos');
+        console.log(sectionTrending);
+
+        //4 Recorrer el array de datos reemplazando la inforamción estátiuca por la data variable.
+        for(let i=0; i<trending.length; i++){
+            sectionTrending.innerHTML += `<article class="media">
+                                                <img src="https://image.tmdb.org/t/p/w500/${trending[i].poster_path}" alt="reemplazar">
+                                                <p class="titulo"> <a href="./moviedetail.html">${trending[i].title}</a> </p>
+                                        </article>`;
+
+        }
+
+    })
+
+    .catch(function(error){
+        console.log(error)
+    })
+
+
+    
