33de61ea3d531d9f04e68dc728f3145ebb2f0b01
solsaha <ssaha@udesa.edu.ar>
2020-11-26 22:51:57 -0300

    terminado

--- html/js/busqueda.js
+++ html/js/busqueda.js
@@ -32,7 +32,7 @@ if(mediaType == "movie"){
             let movies = document.querySelector('.movies');
             console.log(movies);
 
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 4; i++){
                 movies.innerHTML += `<article class="busqueda">
                                     <div class="divbusqueda">
                                         <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
@@ -50,16 +50,15 @@ if(mediaType == "movie"){
 
 }
 
- if(mediaType == "tv"){
+if(mediaType == "tv"){
 
-    let url = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&language=en-US&page=${searchData}1&include_adult=false` //Viene de la API de TMDB
+    let url = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&language=en-US&query=${searchData}&page=1&include_adult=false` //Viene de la API de TMDB
 
     fetch(url)
         .then(function (respuestas) {
             return respuestas.json()
         })
         .then(function (data) {
-            
             //Aca operamos con los datos.
             console.log(data);
 
@@ -71,9 +70,10 @@ if(mediaType == "movie"){
                 movies.innerHTML += `<article class="busqueda">
                                     <div class="divbusqueda">
                                         <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
-                                        
-                                                <a href="moviedetail.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
-                                        
+                                           
+                                             
+                                         <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                                          
                                         </div>
                                     </article>`
             }
@@ -85,13 +85,43 @@ if(mediaType == "movie"){
 }
 
 if(mediaType == "person"){
-    let url = `https://api.themoviedb.org/3/search/person?api_key=${apiKey}&language=en-US&page=${searchData}1&include_adult=false`
+
+   let url =`https://api.themoviedb.org/3/search/person?api_key=${apiKey}&language=en-US&page=${searchData}1&include_adult=false`
+   fetch(url)
+   .then(function (respuestas) {
+       return respuestas.json()
+   })
+   .then(function (data) {
+       //Aca operamos con los datos.
+       console.log(data);
+
+       let info = data.results //Esto es un array.
+       let movies = document.querySelector('.movies');
+       console.log(movies);
+
+       for (let i = 0; i < 4; i++) {
+           movies.innerHTML += `<article class="busqueda">
+                               <div class="divbusqueda">
+                                   <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
+                                      
+                                        
+                                    <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                                     
+                                   </div>
+                               </article>`
+       }
+
+   })
+   .catch(function (error) {
+       console.log(error);
+   })
 }
+
 
 if(mediaType == "all"){
     //fetch a multisearch
 
-let url =`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${searchData}&page=1&include_adult=false`//Viene de la API de TMDB
+    let url = ` https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${searchData}&page=1&include_adult=false` //Viene de la API de TMDB
 
     fetch(url)
         .then(function (respuestas) {
@@ -108,45 +138,39 @@ let url =`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=e
             for (let i = 0; i < info.length; i++) {
                 if(info[i].media_type == "movie"){
                     movies.innerHTML += `<article class="busqueda">
-                                        <div class="divbusqueda">
-                                            <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
-                                              
-    
-                                                    <a href="moviedetail.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
-                                                
-                                            </div>
-                                        </article>`
+                    <div class="divbusqueda">
+                        <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
+                           
+                             
+                         <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                          
+                        </div>
+                    </article>`
                 } else if (info[i].media_type == "tv"){
                     movies.innerHTML += `<article class="busqueda">
-                                        <div class="divbusqueda">
-                                            <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
-                                    
-                                                    <a href="moviedetail.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
-                                                </div>
-                                         
-                                        </article>`
+                    <div class="divbusqueda">
+                        <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
+                           
+                             
+                         <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                          
+                        </div>
+                    </article>`
                 } else {
                     movies.innerHTML += `<article class="busqueda">
-                                    <div class="divbusqueda">
-                                        <img src="https://image.tmdb.org/t/p/w500${info[i].profile_path}" class="card-img-top" alt="...">
-                                        
-                                              
-                                                <a href="moviedetail.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
-                                        
-                                        </div>
-                                    </article>`
+                    <div class="divbusqueda">
+                        <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
+                           
+                             
+                         <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                          
+                        </div>
+                    </article>`
                 }
             }
-        
 
         })
         .catch(function (error) {
             console.log(error);
         })
-
-    }
-
-// window.addEventListener('load',function(){
-//             let spinner = docuent.querySelector('ruedita')
-//             spinner.style.display="none"
-//         })
+}
