de4395ff27a623dede2a0c656afa712641da50ee
solsaha <ssaha@udesa.edu.ar>
2020-11-27 09:48:55 -0300

    mas

--- html/js/busqueda.js
+++ html/js/busqueda.js
@@ -152,7 +152,7 @@ if(mediaType == "all"){
                         <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="No hay imagen disponible">
                            
                              
-                         <a href="moviedetail.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                         <a href="seriedetail.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
                           
                         </div>
                     </article>`
